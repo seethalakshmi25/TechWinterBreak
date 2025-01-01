de91dc8e9a2fb26612deb0f836263ffd0f96c0ee
MuLearn PRN <mulearncep@gmail.com>
2025-01-01 22:50:45 +0530

    Create 09_Multiply_Arrow_Function.js

---
+++ Questions/09_Multiply_Arrow_Function.js
@@ -0,0 +1,12 @@
+// Question: Write an arrow function that accepts two numbers and returns their product. 
+// Use an arrow function to define it.
+
+
+// Write Arrow funtion code here
+
+
+
+console.log(multiply(4, 5)); 
+// Expected Output: 20
+console.log(multiply(2, 3)); 
+// Expected Output: 6
