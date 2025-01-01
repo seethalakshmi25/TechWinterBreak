be8499b0ef9b9daebc302cfcb3903e95cd907e72
MuLearn PRN <mulearncep@gmail.com>
2025-01-01 22:41:04 +0530

    Update 06_Sum_Even_Odd.js

--- Questions/06_Sum_Even_Odd.js
+++ Questions/06_Sum_Even_Odd.js
@@ -1,11 +1,15 @@
 // Question 6: 
-//Write a function that takes two numbers as parameters. If both are even, return their sum; if both are odd, return their difference 
+//Write a function named sumOrDifference that takes two numbers as parameters. If both are even, return their sum; if both are odd, return their difference 
 //and if both are different return -1.
 
-function sumOrDifference(num1, num2) {
-  // Write code here
-}
+// Write code here
 
+
+
+
+
+
+//for testing outputs
 console.log(sumOrDifference(2, 4)); 
 // Expected Output: 6 (Even sum)
 console.log(sumOrDifference(3, 5)); 
