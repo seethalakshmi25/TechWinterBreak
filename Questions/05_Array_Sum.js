9fc3e8722bf1b6a0d8fac55207324d1ce9c0e094
MuLearn PRN <mulearncep@gmail.com>
2025-01-01 22:27:29 +0530

    Create 05_Array_Sum.js

---
+++ Questions/05_Array_Sum.js
@@ -0,0 +1,8 @@
+// Question 6: Write a function that calculates the sum of all numbers in an array.
+
+function calculateSum(numbers) {
+  // Write code here
+}
+
+console.log(calculateSum([1, 2, 3, 4, 5])); 
+// Expected Output: 15
