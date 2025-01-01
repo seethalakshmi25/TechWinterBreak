c823c671d9d26980ec3de62ddfea82f2cd3ab56b
MuLearn PRN <mulearncep@gmail.com>
2025-01-01 22:22:58 +0530

    Update 04_Largest_number.js

--- Questions/04_Largest_number.js
+++ Questions/04_Largest_number.js
@@ -1,1 +1,8 @@
+// Question 4: Write a function that finds and returns the largest number in an array.
 
+function findLargestNumber(numbers) {
+  // Write code here
+}
+
+console.log(findLargestNumber([10, 20, 5, 8])); 
+// Expected Output: 20
