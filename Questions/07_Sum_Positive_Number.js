ce8e0ad9b4bbd64e2f996eb9c425106fe319258e
MuLearn PRN <mulearncep@gmail.com>
2025-01-01 22:43:04 +0530

    Update 07_Sum_Positive_Number.js

--- Questions/07_Sum_Positive_Number.js
+++ Questions/07_Sum_Positive_Number.js
@@ -1,11 +1,13 @@
-// Question 7:
+// Question 7: Write a function named sumPostitveArray which accepts a parameter
 //If the array contains any negative numbers, return the sum of all positive numbers only.
 
-function sumPositiveArray(arr) {
-  // Write code here
-}
+// Write function here
 
 
+
+
+
+//for testing the output
 console.log(sumPositiveArray([1, -2, 3, 4, -5])); 
 // Expected Output: 8 (1 + 3 + 4)
 console.log(sumPositiveArray([-1, -2, -3, -4])); 
