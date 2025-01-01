ec91c1eaa94a537ac69b7f031474ad873814c3cc
MuLearn PRN <mulearncep@gmail.com>
2025-01-01 22:16:01 +0530

    Create 03_loop_through.js

---
+++ Questions/03_loop_through.js
@@ -0,0 +1,12 @@
+// Question 3: Create a function that accepts an array of numbers and prints each number.
+
+function printNumbers(numbers) {
+  // Write code here
+}
+printNumbers([1, 2, 3, 4, 5]); 
+// Expected Output: 
+// 1
+// 2
+// 3
+// 4
+// 5
