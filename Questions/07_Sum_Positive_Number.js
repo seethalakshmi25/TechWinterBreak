639980eb196465bb7812f9b6788d02cc9bd9c86e
MuLearn PRN <mulearncep@gmail.com>
2025-01-01 22:36:14 +0530

    Create 07_Sum_Positive_Number.js

---
+++ Questions/07_Sum_Positive_Number.js
@@ -0,0 +1,12 @@
+// Question 7:
+//If the array contains any negative numbers, return the sum of all positive numbers only.
+
+function sumPositiveArray(arr) {
+  // Write code here
+}
+
+
+console.log(sumPositiveArray([1, -2, 3, 4, -5])); 
+// Expected Output: 8 (1 + 3 + 4)
+console.log(sumPositiveArray([-1, -2, -3, -4])); 
+// Expected Output: 0
