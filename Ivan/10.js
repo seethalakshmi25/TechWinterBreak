b7fb5639138ab27506adf29bfc606f829486bd6d
I_VA_N <150933580+IVANOO7@users.noreply.github.com>
2025-01-02 21:21:58 +0530

    Create 10.js

---
+++ Ivan/10.js
@@ -0,0 +1,17 @@
+// Question 10: Write an arrow function that checks if a number is even or odd.
+// It should return "Even" if the number is even and "Odd" if the number is odd.
+
+// Write Arrow Function code here
+
+const checkEvenOdd = a => {
+    if(a%2 == 0)
+        return "Even";
+    else
+        return "Odd"
+}
+
+
+console.log(checkEvenOdd(4)); 
+// Expected Output: Even
+console.log(checkEvenOdd(7)); 
+// Expected Output: Odd
