b91fabd301cb6b49e52231a816439079669baba6
I_VA_N <150933580+IVANOO7@users.noreply.github.com>
2025-01-02 20:46:29 +0530

    Delete Ivan/01_js.js

--- Ivan/01_js.js
+++
@@ -1,9 +0,0 @@
-// Question 1: Print "Hello, World!" to the console.
-
-
-function printHelloWorld() {
-    console.log("Hello, World!");
-  }
-  
-  printHelloWorld(); 
-  // Expected Output: Hello, World!
