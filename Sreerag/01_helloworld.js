dc914999c765760a7cc2ec62567396e78cf063f8
Sundara Siva Sreerag <151757051+9sreerag7@users.noreply.github.com>
2025-01-02 22:19:52 +0530

    Update 01_helloworld.js

--- Sreerag/01_helloworld.js
+++ Sreerag/01_helloworld.js
@@ -1,1 +1,10 @@
+// Question 1: Print "Hello, World!" to the console.
 
+
+function printHelloWorld() {
+  // Write code here
+  console.log("hello world!");
+}
+
+printHelloWorld(); 
+// Expected Output: Hello, World!
