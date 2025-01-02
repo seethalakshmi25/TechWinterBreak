4b88e0ea154ba57b6b14e769f5be47ef485d463f
MuLearn PRN <mulearncep@gmail.com>
2025-01-02 10:18:53 +0530

    Create 14_Fizz_Buzz.js

---
+++ Questions/14_Fizz_Buzz.js
@@ -0,0 +1,31 @@
+//Given an integer n, for every positive integer i <= n, the task is to print,
+
+// “FizzBuzz” if i is divisible by 3 and 5,
+// “Fizz” if i is divisible by 3,
+// “Buzz” if i is divisible by 5
+// “i” as a string(example : 7 as "7"), if none of the conditions are true.
+
+/*Input: n = 3
+Output: [“1”, “2”, “Fizz”]
+
+
+Input: n = 10
+Output: [“1”, “2”, “Fizz”, “4”, “Buzz”, “Fizz”, “7”, “8”, “Fizz”, “Buzz”]
+
+
+Input: n = 20
+Output: [“1”, “2”, “Fizz”, “4”, “Buzz”, “Fizz”, “7”, “8”, “Fizz”, “Buzz”, “11”, “Fizz”, “13”, “14”, “FizzBuzz”, “16”, “17”, “Fizz”, “19”, “Buzz”]
+*/
+
+
+function fizzBuzz(n) {
+  //write code here  
+}
+
+
+
+
+// Driver code
+let n = 20;
+let res = fizzBuzz(n);
+console.log(res.join(' '));
