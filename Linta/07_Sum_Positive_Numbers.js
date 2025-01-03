8a2caa5b0c74b8c703eba31d919dd95bf5a946ca
linta0987 <166148630+linta0987@users.noreply.github.com>
2025-01-03 22:39:12 +0530

    Create 07_Sum_Positive_Numbers.js

---
+++ Linta/07_Sum_Positive_Numbers.js
@@ -0,0 +1,22 @@
+// Question 7: Write a function named sumPostitveArray which accepts a parameter
+//If the array contains any negative numbers, return the sum of all positive numbers only.
+
+// Write function here
+function sumPositiveArray(numbers)
+{
+    let S=0;
+    for(i=0;i<numbers.length;i++)
+        if(numbers[i]>0)
+            S=S+numbers[i];
+        return S;
+}
+
+
+
+
+
+//for testing the output
+console.log(sumPositiveArray([1, -2, 3, 4, -5])); 
+// Expected Output: 8 (1 + 3 + 4)
+console.log(sumPositiveArray([-1, -2, -3, -4])); 
+// Expected Output: 0
