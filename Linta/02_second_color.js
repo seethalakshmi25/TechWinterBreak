3dd7346a4be152dd4214c1fb5d77fdec6fa4f8b5
linta0987 <166148630+linta0987@users.noreply.github.com>
2025-01-03 21:59:08 +0530

    Create 02_second_color.js

---
+++ Linta/02_second_color.js
@@ -0,0 +1,9 @@
+// Question 2: Create a function that returns the second color from an array passed as a parameter.
+
+
+function getSecondColor(colors) {
+    return colors[1];
+  }
+  
+  console.log(getSecondColor(["red", "blue", "green", "yellow", "purple"])); 
+  // Expected Output: blue
