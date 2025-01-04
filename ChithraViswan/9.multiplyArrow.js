a091e1ecb20b950fac2add727efa68cd67dc041d
chithraviswan <chithraviswan7@gmail.com>
2025-01-04 17:51:15 +0530

    Create 9.multiplyArrow.js

---
+++ ChithraViswan/9.multiplyArrow.js
@@ -0,0 +1,9 @@
+// Question 9: Write an arrow function that accepts two numbers and returns their product. 
+// Use an arrow function to define it.
+
+let multiply=(num1,num2)=>num1*num2;
+
+console.log(multiply(4, 5)); 
+// Expected Output: 20
+console.log(multiply(2, 3)); 
+// Expected Output: 6
